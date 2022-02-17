be5414c2a20d5037522e7bdaa1f521314dfff2ee
dam2a-2021 <nmoyag01@elpuig.xeill.net>
2022-02-17 19:59:09 +0100

    Acabar beta Inventory y diseño Mapa beta

--- app/src/main/java/adventurelegend/game/fragments/InventoryWeaponsFragment.java
+++ app/src/main/java/adventurelegend/game/fragments/InventoryWeaponsFragment.java
@@ -12,10 +12,10 @@ import adventurelegend.game.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link InventoryFragment#newInstance} factory method to
+ * Use the {@link InventoryWeaponsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class InventoryFragment extends Fragment {
+public class InventoryWeaponsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class InventoryFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public InventoryFragment() {
+    public InventoryWeaponsFragment() {
         // Required empty public constructor
     }
 
@@ -39,8 +39,8 @@ public class InventoryFragment extends Fragment {
      * @return A new instance of fragment InventoryFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static InventoryFragment newInstance(String param1, String param2) {
-        InventoryFragment fragment = new InventoryFragment();
+    public static InventoryWeaponsFragment newInstance(String param1, String param2) {
+        InventoryWeaponsFragment fragment = new InventoryWeaponsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class InventoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inventory, container, false);
+        return inflater.inflate(R.layout.fragment_inventory_weapons, container, false);
     }
 }
